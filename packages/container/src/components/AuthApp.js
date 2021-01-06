ef06351a47d78911b743f761276fb8efea0e58d5
Jaskaranpreet Singh <jaskaransingh123@gmail.com>
2021-01-05 22:43:23 -0800

    added auth subapp

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -1,13 +1,13 @@
-import { mount } from 'marketing/MarketingApp';
-import React, { useRef, useEffect } from 'react';
+import { mount } from 'auth/AuthApp';
+import React, { useRef, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom'
 
-export default () => {
+export default ({onSignIn}) => {
   const ref = useRef(null);
   const history = useHistory()
 
   useEffect(() => {
-    const { onParentNavigate } = mount(ref.current,{
+    const { onParentNavigate } = mount(ref.current, {
       initialPath: history.location.pathname,
       onNavigate: ({ pathname : nextPathname }) => { //listen function give an argumnet in marketing, destruct and rename { old name : newname }
         const { pathname } = history.location
@@ -15,7 +15,8 @@ export default () => {
         if( pathname !== nextPathname ){
           history.push(nextPathname)
         }
-      }
+      },
+      onSignIn
     });
 
     history.listen(onParentNavigate)
